27002bf351ef8e3974a3cb87af9a624e86b0b6b2
orcunturkol <56793041+orcunturkol@users.noreply.github.com>
2021-07-24 16:28:11 +0300

    Projects section is completed

---
+++ .next/static/webpack/webpack.0f07b52836659aa0c04f.hot-update.js
@@ -0,0 +1,12 @@
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "6db6d96a9a5cf0a22701"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7VUFBQSxvQ0FBb0MsK0JBQStCLEUiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay4wZjA3YjUyODM2NjU5YWEwYzA0Zi5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcIjZkYjZkOTZhOWE1Y2YwYTIyNzAxXCI7IH0iXSwic291cmNlUm9vdCI6IiJ9
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,16 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle>
+      Full stack developer <br/>
+      and designer  
+      </SectionTitle>
+      <SectionText>I develop good-looking, responsive apps and websites. </SectionText>
+      <Button onClick={()=> window.location = "mailto:ofturkol@gmail.com"}>Contact Me</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
