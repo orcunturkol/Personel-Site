168df533284ce3a4f47848e2c0b4a7158dd7f611
orcunturkol <56793041+orcunturkol@users.noreply.github.com>
2021-07-24 16:42:02 +0300

    Technologies section is complated

---
+++ .next/static/webpack/webpack.8c6d8bde7790f687ae70.hot-update.js
@@ -0,0 +1,12 @@
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "756d12d313d06f60b853"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7VUFBQSxvQ0FBb0MsK0JBQStCLEUiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay44YzZkOGJkZTc3OTBmNjg3YWU3MC5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcIjc1NmQxMmQzMTNkMDZmNjBiODUzXCI7IH0iXSwic291cmNlUm9vdCI6IiJ9
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiAndroid, DiFirebase, DiMongodb, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+ <Section id="tech">
+   <SectionDivider/>
+  <SectionTitle>Technologies</SectionTitle>
+  <SectionText>I have worked with many technologies in the field of web development. From Back-end to design.</SectionText>
+  <List>
+    <ListItem>
+      <DiReact size="3rem"/>
+      <ListContainer>
+        <ListTitle>Front-End</ListTitle>
+        <ListParagraph>Experince with <br/>
+        React.js
+        </ListParagraph>
+      </ListContainer>
+    </ListItem>
+    
+    <ListItem>
+      <DiMongodb size="3rem"/>
+      <ListContainer>
+        <ListTitle>Back-End</ListTitle>
+        <ListParagraph>Experince with <br/>
+        Node.js and Databases
+        </ListParagraph>
+      </ListContainer>
+    </ListItem>
+
+    <ListItem>
+      <DiAndroid size="3rem"/>
+      <ListContainer>
+        <ListTitle>Mobile App</ListTitle>
+        <ListParagraph>Experince with <br/>
+        Flutter
+        </ListParagraph>
+      </ListContainer>
+    </ListItem>
+
+  </List>
+ </Section>
 );
 
 export default Technologies;
